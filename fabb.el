31e07b8f4c4c4684329bb7c6f07659930440bac0
Russell Matney <russell.matney@gmail.com>
2022-05-22 08:44:06 -0400

    feat: parsing nearest bb.edn, building task-defs
    A few functions for determining the nearest bb.edn to parse, parsing it,
    and building task-defs from the bb.edn's `:task` map.

--- fabb.el
+++ fabb.el
@@ -25,13 +25,66 @@
 ;;  from *fabb-tasks*.
 ;;
 ;;; Code:
+(require 's)
+(require 'parseedn)
+(require 'cl-lib)
 
 (defmacro comment (&rest _)
   "Comment out one or more s-expressions."
   nil)
 
+
+(defun find-file-in-current-project (filename)
+  "Find and return a full path for the passed FILENAME."
+  (when-let ((project-dir (locate-dominating-file default-directory filename)))
+    (s-concat (file-name-as-directory project-dir) filename)))
+
 (comment
- (print "Howdy"))
+ (find-file-in-current-project "bb.edn")
+ (find-file-in-current-project "idontexist.edn"))
+
+(defun parse-edn (path)
+  "Parse the passed PATH as edn via `parseedn-read'."
+  (with-temp-buffer
+    (insert-file-contents path)
+    (car (parseedn-read))))
+
+(comment
+ (let ((content
+        (thread-first "bb.edn" find-file-in-current-project parse-edn)))
+   (gethash :tasks content)))
+
+(defun raw-task->task-def (task-name raw-task)
+  "Convert a passed TASK-NAME and RAW-TASK into a fabb-task-def.
+
+task-defs are property lists with these keys:
+- `:task-name': the key from the bb.edn tasks map. Used to invoke the task via
+  `bb <task-name>'.
+- `:task-doc': an optional documentation string from the task's `:doc' key.
+- `:task-command': the clojure s-expression that will eventually be executed."
+  (let* ((is-table (hash-table-p raw-task))
+         (doc (when is-table (gethash :doc raw-task)))
+         (task-def
+          (list :task-name task-name
+                ;; :raw-task raw-task
+                )))
+    (append task-def
+            (when doc (list :task-doc doc))
+            (list :task-command (if is-table
+                                    (gethash :task raw-task)
+                                  raw-task)))))
+
+(defun parse-bb-edn-tasks (path)
+  "Parse and return task-defs for tasks in the bb.edn at PATH."
+  (let* ((bb-edn-content (parse-edn path))
+         (raw-tasks (gethash :tasks bb-edn-content))
+         (task-defs (cl-loop for raw-task being each hash-value of raw-tasks
+                             using (hash-key task-name)
+                             collect (raw-task->task-def task-name raw-task))))
+    task-defs))
+
+(comment
+ (thread-first "bb.edn" find-file-in-current-project parse-bb-edn-tasks))
 
 
 (provide 'fabb)
