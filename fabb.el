3e9b9ed500f338e57401b3e762b1d9e6b1482055
Russell Matney <russell.matney@gmail.com>
2022-05-22 09:27:55 -0400

    feat: select task via ivy, include path/dir on task-def
    Creates a few sections for the code as it expands. Extends the task-def
    parse to include bb.edn path, dir, and project name. Implements an ivy
    interface.

--- fabb.el
+++ fabb.el
@@ -27,12 +27,16 @@
 ;;; Code:
 (require 's)
 (require 'parseedn)
+
 (require 'cl-lib)
+;; maybe this is preferred?
+;; (eval-when-compile (require 'cl-lib))
 
 (defmacro comment (&rest _)
   "Comment out one or more s-expressions."
   nil)
 
+;;; misc helpers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun find-file-in-current-project (filename)
   "Find and return a full path for the passed FILENAME."
@@ -54,18 +58,28 @@
         (thread-first "bb.edn" find-file-in-current-project parse-edn)))
    (gethash :tasks content)))
 
-(defun raw-task->task-def (task-name raw-task)
+;;; parsing task-defs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun raw-task->task-def (task-name raw-task bb-edn-path)
   "Convert a passed TASK-NAME and RAW-TASK into a fabb-task-def.
 
 task-defs are property lists with these keys:
 - `:task-name': the key from the bb.edn tasks map. Used to invoke the task via
   `bb <task-name>'.
 - `:task-doc': an optional documentation string from the task's `:doc' key.
-- `:task-command': the clojure s-expression that will eventually be executed."
-  (let* ((is-table (hash-table-p raw-task))
+- `:task-command': the clojure s-expression that will eventually be executed.
+- `:task-dir': the directory the bb.edn file was in.
+- `:task-dir-name': the name of the parent directory of the bb.edn file.
+- `:task-bb-edn-path': the path to this task's bb.edn."
+  (let* ((bb-edn-dir (directory-file-name (file-name-directory bb-edn-path)))
+         (dir-name (file-name-base bb-edn-dir))
+         (is-table (hash-table-p raw-task))
          (doc (when is-table (gethash :doc raw-task)))
          (task-def
           (list :task-name task-name
+                :task-bb-edn bb-edn-path
+                :task-dir bb-edn-dir
+                :task-dir-name dir-name
                 ;; :raw-task raw-task
                 )))
     (append task-def
@@ -80,11 +94,51 @@ task-defs are property lists with these keys:
          (raw-tasks (gethash :tasks bb-edn-content))
          (task-defs (cl-loop for raw-task being each hash-value of raw-tasks
                              using (hash-key task-name)
-                             collect (raw-task->task-def task-name raw-task))))
+                             collect (raw-task->task-def task-name raw-task path))))
     task-defs))
 
 (comment
  (thread-first "bb.edn" find-file-in-current-project parse-bb-edn-tasks))
+
+(defun fabb-task-defs ()
+  "Return a list of task-defs for the current context."
+  (let ((bb-edn-path (find-file-in-current-project "bb.edn")))
+    (parse-bb-edn-tasks bb-edn-path)))
+
+;;; invoking tasks ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun fabb-invoke-task (task-def)
+  "Invoke the passed TASK-DEF via 'bb <task-name>'."
+  (print task-def))
+
+;;; ivy-frontend ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar fabb-invoke--ivy-history nil
+  "History for `fabb-invoke--ivy'.")
+
+(defun fabb-invoke--ivy-targets ()
+  "Parse task-defs and map them into ivy targets."
+  (thread-last
+    (fabb-task-defs)
+    (mapcar (lambda (task-def)
+              ;; assumes task-names are unique (which they are, per-project)
+              (cons (plist-get task-def :task-name) task-def)))))
+
+(defun fabb-invoke--ivy-action (selection)
+  "Invokes the task-def on SELECTION."
+  (when-let ((selected-task-def (cdr selection)))
+    (fabb-invoke-task selected-task-def)))
+
+(defun fabb-invoke--ivy ()
+  "Select and invoke a bb-task via ivy."
+  (interactive)
+  ;; TODO handle conditionally requiring/configuring ivy?
+  (ivy-read "Invoke bb task:"
+            (fabb-invoke--ivy-targets)
+            :require-match t
+            :caller 'fabb-invoke--ivy
+            :history 'fabb-invoke--ivy-history
+            :action 'fabb-invoke--ivy-action))
 
 
 (provide 'fabb)
